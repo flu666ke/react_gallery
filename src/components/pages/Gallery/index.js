083818913ef71ce7c25bc347b51a60921dfc9194
Slysparrow88 <pavlusha198806@ggmail.com>
2019-06-09 21:24:31 +0300

    create gallery list

--- src/components/pages/Gallery/index.js
+++ src/components/pages/Gallery/index.js
@@ -1,18 +1,37 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import List from './views/List';
+import { connect } from 'react-redux';
+import { getPhotos } from './service';
 import './style.scss';
 
 
-function Gallery() {
+function Gallery(props) {
 
-  
+  const { setPhotos, photos } = props;
+
+  useEffect( () => {
+    getPhotos()
+      .then(photos => {
+        setPhotos(photos);
+      });
+
+  }, []);
 
   return (
     <div className="gallery">
-      <h1>Gallery</h1>
- 
+      <List photos={photos} />
     </div>
   );
 }
 
-export default Gallery;
+
+const mapStateToProps = state => ({
+  photos: state.gallery.photos
+});
+
+const mapDispatchToProps = dispatch => ({
+  setPhotos: photos => dispatch({type: 'SET_PHOTOS', photos})
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(Gallery);
 
--- src/core/RootReducer.js
+++ src/core/RootReducer.js
@@ -2,9 +2,10 @@ import { combineReducers } from 'redux';
 
 
 import app from '../components/App/redux/reducer';
+import gallery from '../components/pages/Gallery/redux/reducer';
 
 
 export default combineReducers({
     app, 
-   
+    gallery
 })
